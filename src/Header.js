19e04c783d25aee942870637d91d9e8e0e642c2e
Seth Schwartz <sschwartz214@gmail.com>
2019-01-17 13:37:32 -0700

    Adds header component

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <i class="fas fa-book-reader" />
+      <h1>BookFindr</h1>
+    </header>
+  );
+};
+
+export default Header;
