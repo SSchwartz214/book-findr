4ea51969c0cfd478faa12141a9ca0afb9b7d398a
Seth Schwartz <sschwartz214@gmail.com>
2019-01-23 19:57:41 -0700

    Adds subtitle

--- src/components/SearchArea.js
+++ src/components/SearchArea.js
@@ -3,6 +3,10 @@ import React from "react";
 const SearchArea = props => {
   return (
     <div className="search-area">
+      <h3>
+        <span className="brand">bookfindr</span> is a free, fast and efficient
+        search-engine for books
+      </h3>
       <form onSubmit={props.searchBook} action="">
         <input
           onChange={props.handleSearch}
